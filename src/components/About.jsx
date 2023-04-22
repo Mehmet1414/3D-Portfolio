4786dc258c6b446043e40681ec5738ef8d738567
Mehmet1414 <wien.2017@outlook.com>
2023-04-22 21:38:10 +0200

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
+  const ServiceCard = ({ index, title, icon }) => {
+    return (
+      <Tilt className="xs:w-[250px] w-full">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+          className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        ></motion.div>
+      </Tilt>
+    );
+  };
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I`m a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I`m a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let`s work together to bring your ideas to life!
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,26 @@ const Hero = () => {
         
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+            animate={{
+              y:[0,24,0]
+            }}
+            transition={{
+              duration:1.9,
+              repeat:Infinity,
+              repeatType:'loop'
+            }}
+            className="w-3 h-3 rounded-full bg-secondary mb-1"
+            
+            />
+
+            
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
