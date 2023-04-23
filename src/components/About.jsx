82124d7059cb07013ba5e757e2c40e2268c5554b
Mehmet1414 <wien.2017@outlook.com>
2023-04-23 04:13:01 +0200

    third commit  1:34:20

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,52 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
-  const ServiceCard = ({ index, title, icon }) => {
-    return (
-      <Tilt className="xs:w-[250px] w-full">
-        <motion.div
-          variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-          className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-        ></motion.div>
-      </Tilt>
-    );
-  };
   return (
-    <>
-      <motion.div variants={textVariant()}>
+    <div className="flex flex-col items-center justify-center">
+      <motion.div variants={textVariant()} 
+      className="flex flex-col items-center justify-center"
+      >
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="px-10 mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] justify-center"
       >
         I`m a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
@@ -36,13 +54,12 @@ const About = () => {
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let`s work together to bring your ideas to life!
       </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-    </>
+    </div>
   );
 };
-
-export default About;
+export default SectionWrapper(About, 'about');
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="rekative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto ">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -26,8 +26,10 @@ const Hero = () => {
 
         
       </div>
+
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center">
+      
+      <div className="absolute bottom-1  w-full flex justify-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
